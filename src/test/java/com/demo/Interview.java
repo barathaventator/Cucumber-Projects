49b4231f3f45099ddadd757ec43f2b22cce451a7
barathaventator <barathaventator@gmail.com>
2023-11-07 16:35:32 +0530

    changes done Tuesday

--- src/test/java/com/demo/Interview.java
+++ src/test/java/com/demo/Interview.java
@@ -5,7 +5,18 @@ public class Interview {
 	public static void main(String[] args) {
 
 		System.out.println("Code in the master");
+		int num = 12345;
+		int rev = 0;
 
+		while (num > 0) {
+
+			int a = num % 10;
+			rev = (rev * 10) + a;
+			num = num / 10;
+
+		}
+
+		System.out.println(rev);
 	}
 
 }
