b1b1f46465772b201241078be84b2ba50f3f2322
barathaventator <96466519+barathaventator@users.noreply.github.com>
2023-11-07 16:39:58 +0530

    Perf (#12)
    * changes done Tuesday
    
    * changes done Tuesday

--- src/test/java/com/demo/Interview.java
+++ src/test/java/com/demo/Interview.java
@@ -5,13 +5,7 @@ public class Interview {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Code in Perf Branch");
-		for (int i = 0; i < 5; i++) {
-		
-			System.out.println("*");
-		}
-		
-	}
+	
 
 	public static void main(String[] args) {
 
@@ -29,5 +23,6 @@ public class Interview {
 
 		System.out.println(rev);
 	}
+
 
 }
